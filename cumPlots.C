3a294240e462379c9591863fb10448ce723b4dfb
Pietro Vischia <pietro.vischia@gmail.com>
2017-02-28 16:05:34 +0100

    Improve cum plots

--- cumPlots.C
+++ cumPlots.C
@@ -1,6 +1,6 @@
 void cumPlots()
 {
-  TFile* f = TFile::Open("/nfs/fanae/user/vischia/www/tth/2017-02-23_22.07/10x10/cumulative_3l.root");
+  TFile* f = TFile::Open("/nfs/fanae/user/vischia/www/tth/2017-02-28_12.00/cumulative_3l.root");
   gStyle->SetOptStat(0);
   TH1F* h = (TH1F*) f->Get("h_cumulative");
   h->GetYaxis()->SetTitle("Cumulative");
@@ -34,6 +34,8 @@ void cumPlots()
     graphs2[bin]->SetPoint(1,xaxis[bin],0.);
   }
 
+  TCanvas* c = new TCanvas("c","c", 800, 800);
+  c->cd();
   h->SetLineWidth(2);
   h->Draw();
   for (int bin = 0; bin < k-1; ++bin){
@@ -44,6 +46,9 @@ void cumPlots()
     graphs1[bin]->Draw("L,same");
     graphs2[bin]->Draw("L,same");
   }
+  gSystem->Exec("mv ~vischia/www/tth/2017-02-28_12.00/cumulative_3l.png ~vischia/www/tth/2017-02-28_12.00/cumulative_3l_ugly.png");
+  gSystem->Exec("mv ~vischia/www/tth/2017-02-28_12.00/cumulative_3l.pdf ~vischia/www/tth/2017-02-28_12.00/cumulative_3l_ugly.pdf");
+  c->Print("~vischia/www/tth/2017-02-28_12.00/cumulative_3l.png");
+  c->Print("~vischia/www/tth/2017-02-28_12.00/cumulative_3l.pdf");
   
-
 }
