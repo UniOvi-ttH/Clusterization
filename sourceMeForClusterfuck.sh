962ee4fb0a668e6be459fa362f2f0dad2d4bbeb2
Pietro Vischia <pietro.vischia@gmail.com>
2018-05-31 18:17:04 +0200

    Automatize running

---
+++ sourceMeForClusterfuck.sh
@@ -0,0 +1,8 @@
+
+.L Significance.C+
+.L RecursiveClustering.C+
+.L LikeliHOOD.C+
+.L clusterfuckOfAwesomeness.C+
+
+clusterfuckOfAwesomeness("2018-05-31_preliminary", "v1");
+cout << "Done clustering 2018-05-31_preliminary" << endl;
